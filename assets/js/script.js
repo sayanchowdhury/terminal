a62842a59f18822ab885be65e48d6ce612ffc4a3
Sayan Chowdhury <sayan.chowdhury2012@gmail.com>
2023-05-30 22:06:04 +0530

    Initial commit
    Signed-off-by: Sayan Chowdhury <sayan.chowdhury2012@gmail.com>

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+var placeholder = document.getElementById('hero-placeholder');
+var typewriter = new Typewriter(placeholder, {
+  wrapperClassName: "hero-placeholder",
+});
+typewriter.typeString('Sayan Chowdhury').start();
